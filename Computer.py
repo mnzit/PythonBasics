e42cd4a4c71f30c9f724e5712d67f1298f462f47
mnzit <mnzitshakya@gmail.com>
2018-08-17 17:16:04 +0545

    OOP Begins

---
+++ Computer.py
@@ -0,0 +1,13 @@
+class Computer:
+    def config(self):
+        print("i5, 16GB, 1TB")
+
+
+com1 = Computer()
+com2 = Computer()
+
+Computer.config(com1)
+Computer.config(com2)
+
+# print(type(com))
+
