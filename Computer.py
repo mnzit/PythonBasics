88a6fbd97adea3bce5fd1e9c3dba80167b73460d
mnzit <mnzitshakya@gmail.com>
2018-08-17 17:29:11 +0545

    Objects Constructor(__init__)

--- Computer.py
+++ Computer.py
@@ -1,13 +1,23 @@
 class Computer:
+    cpu = ''
+    ram = ''
+
+    def __init__(self, cpu, ram):
+        self.cpu = cpu
+        self.ram = ram
+
     def config(self):
-        print("i5, 16GB, 1TB")
+        print("Config is ", self.cpu, self.ram)
 
 
-com1 = Computer()
-com2 = Computer()
+com1 = Computer('i5', '16GB')
+com2 = Computer('i7', '8GB')
 
-Computer.config(com1)
-Computer.config(com2)
+# Computer.config(com1)
+# Computer.config(com2)
+
+com1.config()
+com2.config()
 
 # print(type(com))
 
