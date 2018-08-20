85644feb8335266933504173045fc44347343cf6
mnzit <mnzitshakya@gmail.com>
2018-08-20 17:32:59 +0545

    After getting more in constructors in Python

--- Computer.py
+++ Computer.py
@@ -1,23 +1,59 @@
-class Computer:
-    cpu = ''
-    ram = ''
-
-    def __init__(self, cpu, ram):
-        self.cpu = cpu
-        self.ram = ram
-
-    def config(self):
-        print("Config is ", self.cpu, self.ram)
-
-
-com1 = Computer('i5', '16GB')
-com2 = Computer('i7', '8GB')
-
-# Computer.config(com1)
-# Computer.config(com2)
-
-com1.config()
-com2.config()
-
-# print(type(com))
-
+# class Computer:
+#
+#     cpu = ''
+#     ram = ''
+#
+#     def __init__(self, cpu, ram):
+#         self.cpu = cpu
+#         self.ram = ram
+#
+#     def config(self):
+#         print("Config is ", self.cpu, self.ram)
+#
+#
+# com1 = Computer('i5', '16GB')
+# com2 = Computer('i7', '8GB')
+#
+# # Computer.config(com1)
+# # Computer.config(com2)
+#
+# com1.config()
+# com2.config()
+#
+# # print(type(com))
+#
+# class Person:
+#
+#     def __init__(self):
+#         self.name = "Manjit"
+#         self.age = 20
+#
+#     def update(self):
+#         self.age = 30
+#
+#     def compare(self, other):
+#         if self.age == other.age:
+#             return True
+#         else:
+#             return False
+#
+#
+# p1 = Person()
+# p2 = Person()
+#
+# # print(id(p1))
+# # print(id(p2))
+#
+#
+# # p1.name = "Himal"
+# # p1.age = 12
+#
+# # p1.update()
+#
+# if p1.compare(p2):
+#     print("Same")
+# else:
+#     print("Different")
+#
+# print(p1.name)
+# print(p1.age)
