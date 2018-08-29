65131a99b1fcd50282754a503cf742fb10de1a5e
mnzit <mnzitshakya@gmail.com>
2018-08-29 16:54:04 +0545

    Inheritance

--- MyCode.py
+++ MyCode.py
@@ -552,6 +552,7 @@ from array import *
 #     return n % 2 == 0
 # nums = [1,2,3,4,5,6]
 # evens = list(filter(lambda n : n % 2 == 0, nums))
+
 # doubles = list(map(lambda n : n * 2, evens))
 # sum = reduce(lambda a, b : a + b, doubles)
 # print(sum)
---
+++ inheritance.py
@@ -0,0 +1,87 @@
+# Single Level Inheritance
+
+# class A:
+#     def feature1(self):
+#         print("Feature 1 is progress")
+#
+#     def feature2(self):
+#         print("Feature 2 is progress")
+#
+#
+# class B(A):
+#     def feature3(self):
+#         print("Feature 3 is progress")
+#
+#     def feature4(self):
+#         print("Feature 4 is progress")
+#
+# b1 = B()
+#
+# b1.feature1()
+# b1.feature2()
+# b1.feature3()
+# b1.feature4()
+
+# Multi Level Inheritance
+
+# class C:
+#     def feature1(self):
+#         print("Feature 1 is progress")
+#
+#     def feature2(self):
+#         print("Feature 2 is progress")
+#
+#
+# class D(C):
+#     def feature3(self):
+#         print("Feature 3 is progress")
+#
+#     def feature4(self):
+#         print("Feature 4 is progress")
+#
+# class E(D):
+#     def feature5(self):
+#         print("Feature 5 is progress")
+#
+#     def feature6(self):
+#         print("Feature 6 is progress")
+#
+#
+# e1 = E()
+#
+# e1.feature1()
+# e1.feature2()
+# e1.feature3()
+# e1.feature4()
+# e1.feature5()
+# e1.feature6()
+
+# Multiple Inheritance
+
+# class X:
+#     def feature1(self):
+#         print("Feature 1 is progress")
+#
+#     def feature2(self):
+#         print("Feature 2 is progress")
+#
+# class Y:
+#     def feature3(self):
+#         print("Feature 3 is progress")
+#
+#     def feature4(self):
+#         print("Feature 4 is progress")
+#
+# class Z(X, Y):
+#     def feature5(self):
+#         print("Feature 5 is progress")
+#
+# z1 = Z()
+#
+# z1.feature1()
+# z1.feature2()
+# z1.feature3()
+# z1.feature4()
+# z1.feature5()
+
+
