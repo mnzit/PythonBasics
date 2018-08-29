46a0db98be9d50fd12a10adad332f0c817a2159d
mnzit <mnzitshakya@gmail.com>
2018-08-29 21:12:15 +0545

    Inheritance and Method resolution order (Left to Right)

--- inheritance.py
+++ inheritance.py
@@ -1,3 +1,4 @@
+# Inheritance
 # Single Level Inheritance
 
 # class A:
@@ -84,4 +85,74 @@
 # z1.feature4()
 # z1.feature5()
 
+# Constructor in inheritance
+# If B doesnt have constructor defined it will call A's Constructor
+
+# class A:
+#     def __init__(self):
+#         print("Print in A Init")
+#
+#     def feature1(self):
+#         print("Feature 1 is progress")
+#
+#     def feature2(self):
+#         print("Feature 2 is progress")
+#
+#
+# class B(A):
+#     def __init__(self):
+#
+#         # Accessing init of super class
+#
+#         super().__init__()
+#         print("Print in B Init")
+#
+#     def feature3(self):
+#         print("Feature 3 is progress")
+#
+#     def feature4(self):
+#         print("Feature 4 is progress")
+#
+# b1 = B()
+
+class A:
+    def __init__(self):
+        print("Print in A Init")
+
+    def feature1(self):
+        print("Feature 1 is progress")
+
+    def feature2(self):
+        print("Feature 2 is progress")
+
+    def newFeature(self):
+        print("And here is our new feature from A")
+
+class B:
+    def __init__(self):
+        print("Print in B Init")
+
+    def feature3(self):
+        print("Feature 3 is progress")
+
+    def feature4(self):
+        print("Feature 4 is progress")
+
+    def newFeature(self):
+        print("And here is our new feature from B")
+
+# Multiple Level inheritance has the feature of MRO (MEthod resoultion order)
+# Left to Right
+# Same Rule to Methods too
+
+class C(A, B):
+    def __init__(self):
+        super().__init__()
+        print("Print in C Init")
+
+    def feat(self):
+        super().feature2()
+
+c1 = C()
+c1.newFeature()
 
